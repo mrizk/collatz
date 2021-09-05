35dd02c6f3be25c40b2e691c54530636faf47edc
Al Moataz Rizk <mor@vioma.de>
2021-09-05 14:48:27 +0200

    read port from env vars

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"collatz/api"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/collatz", api.CollatzHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
